0673f369e8e37d5039250969576c7b0809c1c0ba
qkrekfthsus <qkrekfthsus@naver.com>
2021-04-09 18:32:19 +0900

    asdf

--- Curtain/main.py
+++ Curtain/main.py
@@ -2,37 +2,44 @@ import pymysql
 import time
 import sys
 import threading
+
+import numpy as np
 """
 각 조명에 대한 평균값 구하기 (알고리즘 설계)
 """
 
 # 5초 마다
 # 5분간 데이터 평균
-def my_data_insert(i):
-    conn = pymysql.connect(host='localhost', user='root', port=3306,
-                           password='0000', db='curtain', charset='utf8')
-    cursor = conn.cursor()
 
-    sql = "SELECT * FROM brightness"
+conn = pymysql.connect(host='localhost', user='root', port=3306,
+                    password='0000', db='curtain', charset='utf8')
+cursor = conn.cursor()
 
-    cursor.execute(sql)
-    res = cursor.fetchall()
+sql = "SELECT * FROM brightness"
 
-    conn.commit()
-    conn.close()
-    return sum([arr[i] for arr in res]) / len(res)
+cursor.execute(sql)
+res = cursor.fetchall()
+conn.commit()
+conn.close()
+
+inside = [data[1] for data in res]
+outside = [data[2] for data in res]
+
+in_average = np.array(inside).mean()
+out_average = np.array(outside).mean()
 
 
 class AverageLight():
     def __init__(self, hope_light):
-        self.inside_light = my_data_insert(1)
-        self.outside_light = my_data_insert(2)
+        self.inside_light = in_average
+        self.outside_light = out_average
         self.hope_list = hope_light
     
 
     def control_function(self):
         print(f"linear average --> {self.inside_light}")
         print(f"outline average --> {self.outside_light}")
+
         if self.inside_light < self.outside_light:
             if self.inside_light == self.hope_list:
                 print("LED right")
@@ -47,5 +54,4 @@ class AverageLight():
                 print("curtain down")
 
 
-if __name__ == "__main__":
-    AverageLight(30).control_function()
+AverageLight(30).control_function()
