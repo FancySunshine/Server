76600814b6a136e85571150646fcfdc7836bee11
kimyoungchang <dudckd6826@github.com>
2021-09-10 14:49:33 +0900

    python modify

--- Curtain/main.py
+++ Curtain/main.py
@@ -2,7 +2,7 @@ import pymysql
 import time
 import sys
 import threading
-
+import json
 import numpy as np
 """
 각 조명에 대한 평균값 구하기 (알고리즘 설계)
@@ -12,11 +12,10 @@ import numpy as np
 # 5분간 데이터 평균
 
 conn = pymysql.connect(host='localhost', user='root', port=3306,
-                    password='0000', db='curtain', charset='utf8')
+                       password='0000', db='curtain', charset='utf8')
 cursor = conn.cursor()
 
 sql = "SELECT * FROM brightness"
-
 
 cursor.execute(sql)
 res = cursor.fetchall()
@@ -29,15 +28,15 @@ outside = [data[2] for data in res]
 in_average = np.array(inside).mean()
 out_average = np.array(outside).mean()
 print('in_average from DB : ', in_average)
-print('out_average from DB : ', out_average )
+print('out_average from DB : ', out_average)
 lux_standard = [0, 100, 200, 300, 400, 500]
+
 
 class AverageLight():
     def __init__(self):
         self.inside_light = in_average
         self.outside_light = out_average
         self.hope_light = lux_standard[int(sys.argv[1])]
-    
 
     def control_function(self):
         print(f"linear average --> {self.inside_light}")
@@ -46,15 +45,29 @@ class AverageLight():
         if self.inside_light < self.outside_light:
             if self.inside_light == self.hope_light:
                 print("Curtain|0.999")
+                up_curtain = {'curtain': 'up'}
+                json_change = json.dumps(up_curtain)
+                print(json.dumps(up_curtain, indent=4))
+                return json_change
             else:
                 print("Curtain| trun off!")
+                down_curtain = {'curtain': 'down'}
+                json_change = json.dumps(down_curtain)
+                print(json.dumps(down_curtain, indent=4))
+                return json_change
         else:
             if self.inside_light == self.hope_light:
                 print("OK")
             elif self.hope_light > self.inside_light:
-                print("LED up")
+                down_led = {'led': 'down'}
+                json_change = json.dumps(down_led)
+                print(json.dumps(down_led, indent=4))
+                return json_change
             else:
-                print("LED down")
+                up_led = {'led': 'up'}
+                json_change = json.dumps(up_led)
+                print(json.dumps(up_led, indent=4))
+                return json_change
 
 
 AverageLight().control_function()
